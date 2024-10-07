5ff1d4ae596575f9a4411669f04cde0723d51512
jcciria <90833417+jcciria@users.noreply.github.com>
2024-10-07 10:07:49 +0200

    Create bolzano.py

---
+++ bolzano.py
@@ -0,0 +1,16 @@
+def bolzano (f, interval, epsilon, **paramF):
+    a,b= [x for x in interval]
+    print (a,b)
+    fa = f(a, **paramF)
+    fb = f(b, **paramF)
+    if fa * fb >=0:
+        sol = np.nan
+    else:
+        while (b - a > epsilon):
+            med = (a+b)/2
+            if f(med, **paramF) * fa > 0:
+                a = med
+            else:
+                b = med
+        sol = (a+b)/2
+    return sol
